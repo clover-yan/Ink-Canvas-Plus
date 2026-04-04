d2b50b51adf2156bd6925dd7a7f6262dcdeca6fc
Clover Yan <i@khyan.top>
2026-04-04 12:58:11 +0800

    fix: 启动时浮动栏自动折叠不工作
    - 移动自动收起浮动栏逻辑至 Window_Loaded。

--- Ink Canvas/MainWindow.InputAndPanels.cs
+++ Ink Canvas/MainWindow.InputAndPanels.cs
@@ -644,6 +644,21 @@ namespace InkCanvasPlus
             {
                 UpdateFloatBarExpansionDirection();
             }), DispatcherPriority.Render);
+
+            if (Settings.Appearance.IsAutoCollapseFloatBar)
+            {
+                new Thread(new ThreadStart(() =>
+                {
+                    Thread.Sleep(3000);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (Main_Grid.Background == Brushes.Transparent)
+                        {
+                            SetBorderFloatingBarMainControlsVisibility(false);
+                        }
+                    });
+                })).Start();
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
--- Ink Canvas/MainWindow.xaml.cs
+++ Ink Canvas/MainWindow.xaml.cs
@@ -81,22 +81,6 @@ namespace InkCanvasPlus
                 ViewBoxStackPanelMain.Visibility = Visibility.Collapsed;
                 ViewBoxStackPanelShapes.Visibility = Visibility.Collapsed;
                 HideSubPanels();
-
-                if (Settings.Appearance.IsAutoCollapseFloatBar)
-                {
-                    new Thread(new ThreadStart(() =>
-                    {
-                        Thread.Sleep(3000);
-                        Application.Current.Dispatcher.Invoke(() =>
-                        {
-                            if (Main_Grid.Background == Brushes.Transparent)
-                            {
-                                SetBorderFloatingBarMainControlsVisibility(false);
-                            }
-                        });
-                    })).Start();
-                }
-
             }
             else
             {
